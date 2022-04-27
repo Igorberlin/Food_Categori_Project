bf7e05fb3936b03f1118759257138d0cbf7a74b2
Igor <igorkuznecov@gmx.de>
2022-04-27 18:26:04 +0200

    create redirect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import './App.css';
-import Categories from './components/Categories';
-import Meals from './components/Meals';
-import { Switch, Route } from 'react-router-dom';
-import MealsDetails from './components/MealsDetails';
-
-
+import "./App.css";
+import Categories from "./components/Categories";
+import Meals from "./components/Meals";
+import { Switch, Route } from "react-router-dom";
+import MealsDetails from "./components/MealsDetails";
 
 const App = () => {
-
     return (
-      <Switch>
-        <Route exact path='/' component={Categories} />
-        <Route exact path='/:category' component={Meals} />
-        <Route path='/:category/:idMeal' component={MealsDetails}/>
-      </Switch>
-    )
-  }
+        <Switch>
+            <Route exact path="/" component={Categories} />
+            <Route exact path="/:category" component={Meals} />
+            <Route path="/:category/:idMeal" component={MealsDetails} />
+        </Switch>
+    );
+};
 
 export default App;
 
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,53 +1,57 @@
-import React, {useState, useEffect} from 'react';
-import Loader from './Loader';
-import Error from './Error';
-import Meal from './Meal';
-import FoodApi from '../data';
-import Navigation from './Navigation';
+import React, { useState, useEffect } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import Meal from "./Meal";
+import FoodApi from "../data";
+import Navigation from "./Navigation";
+import { Redirect } from "react-router-dom";
 
 const Meals = ({ match }) => {
-    
     const [loading, setLoading] = useState(true);
     const [meals, setMeals] = useState([]);
-    const [error, setError] = useState(null)
+    const [error, setError] = useState(null);
 
     const category = match.params.category;
 
     useEffect(() => {
         setLoading(true);
-        setError(null)
+        setError(null);
         FoodApi.getMealsByCategory(category)
-            .then(data => {
+            .then((data) => {
                 setTimeout(() => {
                     if (data.meals) {
                         setLoading(false);
-                        setMeals(data.meals)
+                        setMeals(data.meals);
                     } else {
-                        setError('no such category');
-                        setLoading(false)
+                        setError("no such category");
+                        setLoading(false);
                     }
-                }, 1000)
+                }, 1000);
             })
             .catch((error) => {
                 setLoading(false);
-                setError(error.message)
-            })
-    }, [category])
+                setError(error.message);
+            });
+    }, [category]);
 
-    
     const renderMeals = () => {
-        return loading && !error ? <Loader /> : error ? <Error message={error} /> :
-            meals.map(meal => <Meal key={meal.idMeal} meal={meal} />)
+        return loading && !error ? (
+            <Loader />
+        ) : error ? (
+            <Error message={error} />
+        ) : (
+            meals.map((meal) => <Meal key={meal.idMeal} meal={meal} />)
+        );
+    };
+    if (category === "Food_Categori_Project") {
+        return <Redirect to="/" />;
     }
-
     return (
-        <div className='container'>
-            <Navigation/>
-            <h2 className='text-primary text-center'>{category}</h2>
-            <div className='row'>
-                {renderMeals()}
-            </div>
+        <div className="container">
+            <Navigation />
+            <h2 className="text-primary text-center">{category}</h2>
+            <div className="row">{renderMeals()}</div>
         </div>
-    )
-}
+    );
+};
 export default Meals;
